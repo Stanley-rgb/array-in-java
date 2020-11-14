31beb611df8971ed9ed32f147f848b2b8a8b423d
unknown <briannjogu42@gmail.com>
2020-11-14 14:58:08 +0300

    first commit

---
+++ src/aray/Aray.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aray;
+
+public class Aray {
+    public static void main(String[] args) {
+        int brian[]=new int[4];
+        brian[0]=1;
+        brian[1]=2;
+        brian[2]=3;
+        brian[3]=4;
+        System.out.println(brian[2]);
+        
+        int njogu[]={12,14,56,78,89,100};
+        System.out.println(njogu[3]);
+    }
+    
+}
